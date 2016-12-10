1c53ea74d55ac4c0ec21fc259510611f26499b52
sgh1992 <1216688393@qq.com>
2016-12-10 10:24:20 +0800

    update in 1209

--- consume/regularity/Test.py
+++ consume/regularity/Test.py
@@ -3,17 +3,20 @@
 import consumeRegularity as regu
 
 startIndex = 3
-term = u'all'
+term = u'1'
 type = u'mess'
 year = u'2010'
-classValue = u'就业'
-
-termList = [u'1',u'2',u'3',u'4',u'5',u'6',u'7',u'8']
+classValue = u'出国出境'
+meal = 'allMeal'
+termList = [u'1', u'2', u'3', u'4', u'5', u'6', u'7', u'8']
 classValueList = [u'就业',u'录研',u'出国出境',u'其它']
+mealList = ['breakfast', 'lunch', 'dinner','allMeal']
+regu.analysizeRegularity(startIndex,term,type,classValue,year,meal)
 
-for classValue in classValueList:
-    regu.analysizeRegularity(startIndex,term,type,classValue,year)
+# for classValue in classValueList:
+#     regu.analysizeRegularity(startIndex,term,type,classValue,year)
 
 # for classValue in classValueList:
 #     for term in termList:
-#         regu.analysizeRegularity(startIndex,term,type,classValue,year)
+#         for meal in mealList:
+#             regu.analysizeRegularity(startIndex,term,type,classValue,year,meal)
--- consume/regularity/consumeRegularity.py
+++ consume/regularity/consumeRegularity.py
@@ -2,8 +2,8 @@
 # -*- coding: utf-8 -*-
 
 import matplotlib.pyplot as plt
-regularityWithWork = 'D:/GraduationThesis/consumeRegularityWithWork.csv'
-def analysizeRegularity(startIndex,term,type,classValue,year='2010'):
+regularityWithWork = '/home/sghipr/consumeRegularityWithWork.csv'
+def analysizeRegularity(startIndex,term,type,classValue,year='2010',meal='allMeal'):
     """
     针对不同的学生群体进行分析
     :param startIndex: 时间段的划分之始.
@@ -11,21 +11,24 @@ def analysizeRegularity(startIndex,term,type,classValue,year='2010'):
     :param type: 消费类别.
     :param classValue:毕业去向群体.
     :param year: 毕业年级.
+    :param meal: 早,午,晚三餐.
     :return:
     """
     data = [line.strip().decode('utf8').split(",") for line in open(regularityWithWork)]
-    timeInterals = getInteralTime()
+    timeInterals = getInteralTime('allMeal')
+    mealInterals = getInteralTime(meal)
     interalLists = sorted(timeInterals.keys())
     for line in data:
         studentID = line[0]
         sterm = line[1]
         stype = line[2]
         sclassValue = line[-1]
-        if condition(studentID,sterm,stype,sclassValue,term,type,classValue,year):
+        if condition(studentID, sterm, stype, sclassValue, term, type, classValue, year):
             for index in range(startIndex,len(line)-1):
-                timeInterals[interalLists[index - startIndex]] += int(line[index])
+                if interalLists[index - startIndex] in mealInterals:
+                    mealInterals[interalLists[index - startIndex]] += int(line[index])
 
-    result = sorted(timeInterals.items(),key= lambda x:x[0])
+    result = sorted(mealInterals.items(), key= lambda x:x[0])
     xList = []
     yList = []
     for x, y in result:
@@ -33,82 +36,121 @@ def analysizeRegularity(startIndex,term,type,classValue,year='2010'):
             xList.append(x)
             yList.append(y)
 
-    sumValue = sum(yList) + 0.0
+    sumValue = getSumValue(data,year,classValue)
     for i in range(0,len(yList)):
         yList[i] /= sumValue
 
-    title = type + ',' + str(term) + ',' + year + ',' + classValue
+    title = type + ',' + str(term) + ',' + year + ',' + classValue + ',' + meal
     plt.figure(figsize=(20,9))
     plt.plot(range(1,len(xList) + 1),yList,color='black', markersize=10)
     plt.grid(True)
     plt.xticks(range(1,len(xList) + 1),xList,rotation=45)
-    plt.xlabel('time interals')
     plt.ylabel('frequences')
     plt.title(title)
-    plt.savefig('D:/GraduationThesis/pictures/' + title + '.pdf')
+    plt.savefig('/home/sghipr/GraduationThesis/pictures/' + title + '.png')
 
 
-def getInteralTime():
+def getSumValue(data,year,classValue,t=180.0):
+    sidSet = set()
+    for line in data:
+        studentID = line[0]
+        sclassValue = line[-1]
+        if classValue == u'allClass':
+            sclassValue = classValue
+        if year == u'2010':
+            if studentID.startswith('2010') and sclassValue == classValue:
+                sidSet.add(studentID)
+        elif year == u'2009':
+            if (not studentID.startswith('2010')) and sclassValue == classValue:
+                sidSet.add(studentID)
+    return len(sidSet) * t
+
+
+def getInteralTime(meal):
     timeInterals = dict()
-    timeInterals['0000-0030'] = 0
-    timeInterals['0030-0100'] = 0
-    timeInterals['0100-0130'] = 0
-    timeInterals['0130-0200'] = 0
-    timeInterals['0200-0230'] = 0
-    timeInterals['0230-0300'] = 0
-    timeInterals['0300-0330'] = 0
-    timeInterals['0330-0400'] = 0
-    timeInterals['0400-0430'] = 0
-    timeInterals['0430-0500'] = 0
-    timeInterals['0500-0530'] = 0
-    timeInterals['0530-0600'] = 0
-    timeInterals['0600-0630'] = 0
-    timeInterals['0630-0700'] = 0
-    timeInterals['0700-0730'] = 0
-    timeInterals['0730-0800'] = 0
-    timeInterals['0800-0830'] = 0
-    timeInterals['0830-0900'] = 0
-    timeInterals['0900-0930'] = 0
-    timeInterals['0930-1000'] = 0
-    timeInterals['1000-1030'] = 0
-    timeInterals['1030-1100'] = 0
-    timeInterals['1100-1130'] = 0
-    timeInterals['1130-1200'] = 0
-    timeInterals['1200-1230'] = 0
-    timeInterals['1230-1300'] = 0
-    timeInterals['1300-1330'] = 0
-    timeInterals['1330-1400'] = 0
-    timeInterals['1400-1430'] = 0
-    timeInterals['1430-1500'] = 0
-    timeInterals['1500-1530'] = 0
-    timeInterals['1530-1600'] = 0
-    timeInterals['1600-1630'] = 0
-    timeInterals['1630-1700'] = 0
-    timeInterals['1700-1730'] = 0
-    timeInterals['1730-1800'] = 0
-    timeInterals['1800-1830'] = 0
-    timeInterals['1830-1900'] = 0
-    timeInterals['1900-1930'] = 0
-    timeInterals['1930-2000'] = 0
-    timeInterals['2000-2030'] = 0
-    timeInterals['2030-2100'] = 0
-    timeInterals['2100-2130'] = 0
-    timeInterals['2130-2200'] = 0
-    timeInterals['2200-2230'] = 0
-    timeInterals['2230-2300'] = 0
-    timeInterals['2300-2330'] = 0
-    timeInterals['2330-0000'] = 0
+    if meal == 'allMeal':
+        timeInterals['0000-0030'] = 0
+        timeInterals['0030-0100'] = 0
+        timeInterals['0100-0130'] = 0
+        timeInterals['0130-0200'] = 0
+        timeInterals['0200-0230'] = 0
+        timeInterals['0230-0300'] = 0
+        timeInterals['0300-0330'] = 0
+        timeInterals['0330-0400'] = 0
+        timeInterals['0400-0430'] = 0
+        timeInterals['0430-0500'] = 0
+        timeInterals['0500-0530'] = 0
+        timeInterals['0530-0600'] = 0
+        timeInterals['0600-0630'] = 0
+        timeInterals['0630-0700'] = 0
+        timeInterals['0700-0730'] = 0
+        timeInterals['0730-0800'] = 0
+        timeInterals['0800-0830'] = 0
+        timeInterals['0830-0900'] = 0
+        timeInterals['0900-0930'] = 0
+        timeInterals['0930-1000'] = 0
+        timeInterals['1000-1030'] = 0
+        timeInterals['1030-1100'] = 0
+        timeInterals['1100-1130'] = 0
+        timeInterals['1130-1200'] = 0
+        timeInterals['1200-1230'] = 0
+        timeInterals['1230-1300'] = 0
+        timeInterals['1300-1330'] = 0
+        timeInterals['1330-1400'] = 0
+        timeInterals['1400-1430'] = 0
+        timeInterals['1430-1500'] = 0
+        timeInterals['1500-1530'] = 0
+        timeInterals['1530-1600'] = 0
+        timeInterals['1600-1630'] = 0
+        timeInterals['1630-1700'] = 0
+        timeInterals['1700-1730'] = 0
+        timeInterals['1730-1800'] = 0
+        timeInterals['1800-1830'] = 0
+        timeInterals['1830-1900'] = 0
+        timeInterals['1900-1930'] = 0
+        timeInterals['1930-2000'] = 0
+        timeInterals['2000-2030'] = 0
+        timeInterals['2030-2100'] = 0
+        timeInterals['2100-2130'] = 0
+        timeInterals['2130-2200'] = 0
+        timeInterals['2200-2230'] = 0
+        timeInterals['2230-2300'] = 0
+        timeInterals['2300-2330'] = 0
+        timeInterals['2330-0000'] = 0
+    elif meal == 'breakfast':
+        timeInterals['0600-0630'] = 0
+        timeInterals['0630-0700'] = 0
+        timeInterals['0700-0730'] = 0
+        timeInterals['0730-0800'] = 0
+        timeInterals['0800-0830'] = 0
+        timeInterals['0830-0900'] = 0
+        timeInterals['0900-0930'] = 0
+    elif meal == 'lunch':
+        timeInterals['1100-1130'] = 0
+        timeInterals['1130-1200'] = 0
+        timeInterals['1200-1230'] = 0
+        timeInterals['1230-1300'] = 0
+        timeInterals['1200-1330'] = 0
+    elif meal == 'dinner':
+        timeInterals['1630-1700'] = 0
+        timeInterals['1700-1730'] = 0
+        timeInterals['1730-1800'] = 0
+        timeInterals['1800-1830'] = 0
+        timeInterals['1830-1900'] = 0
+    else:
+        print meal + ' is not right format!'
     return timeInterals
-
 
 def condition(studentID,sterm,stype,sclassValue,term,type,classValue,year):
     flag = False
-    if year == '2009':
+    if year == u'2009':
         flag = studentID.startswith('2009') and studentID.startswith('29')
-    elif year == '2010':
+    elif year == u'2010':
         flag = studentID.startswith('2010')
 
-    if term == u'all':
+    if classValue == u'allClass':
+        sclassValue = classValue
+    if term == u'allTerm':
         sterm = term
 
     if flag:
